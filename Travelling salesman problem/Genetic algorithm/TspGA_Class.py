67aaac76917972cf8442d6dc45252c1c92e6502d
Marcus <marcusgreiff.93@hotmail.com>
2015-09-27 19:45:14 +0200

    Update algorithm to store the solution history and fixed plot fucntion

--- Travelling salesman problem/Genetic algorithm/TspGA_Class.py
+++ Travelling salesman problem/Genetic algorithm/TspGA_Class.py
@@ -8,37 +8,47 @@ import copy as copy
 
 class TspGA(object):
     
-    def __init__(self, dataX, dataY):
+    def __init__(self, dataX, dataY, ):
         """
         Args:
             dataX (array): A 1xN array of x-points.
             dataY (array): A 1xN array of y-points.
         """
         self.xy = np.array([dataX, dataY])
-        self.populationSize = 48
-        self.iterationLim = 50
+        self.populationSize = 100
+        self.iterationLim = 1000
+        self.solutionHistory = []
+        self.path = []
         self.dist = self._distance_matrix()
 
     def __call__(self):
         print 'not yet implemented'
 
-    def plot(self, points = 0, distance = 0, solution = 0):
+    def plot(self, points = 0, distance = 0, solution = 0, solutionHistory = 0):
         """
         Args:
-            points (bool): Set totrue if the XY points are to be plotted
+            points (bool): Set to true if the XY points are to be plotted
         """
         if solution:
             for ii in range(len(self.path) - 1):
                 plt.gca().plot([self.xy[0][self.path[ii]], self.xy[0][self.path[ii + 1]]],
                                [self.xy[1][self.path[ii]], self.xy[1][self.path[ii + 1]]], 'k')
+            plt.xlabel('x'), plt.ylabel('y'), plt.title('Solution')
         if points:
-            plt.gca().plot(self.xy[0], self.xy[1],'ro', markersize=5)         
+            plt.gca().plot(self.xy[0], self.xy[1],'ro', markersize=5)
+            plt.xlabel('x'), plt.ylabel('y'), plt.title('Input points')
             for i in range(len(self.xy[0])):
                 plt.gca().annotate("#" + str(i), (self.xy[0][i],self.xy[1][i]))
-                
         if distance:
             plt.gca().matshow(self.dist)
-            
+            plt.title('Distance matrix')
+        if solutionHistory:
+            plt.gca().plot([ii for ii in range(self.iterationLim)], self.solutionHistory)
+            plt.title('Solution history')
+            plt.ylabel('Cost'), plt.xlabel('Number of iterations')
+        if solution and points:
+            plt.title('Input points and solution')
+        plt.show()
 
     def _distance_matrix(self):
         """
@@ -66,6 +76,10 @@ class TspGA(object):
             populationDist = np.array([sum([self.dist[population[jj][ii - 1], population[jj][ii]] for ii in range(n)]) for jj in range(self.populationSize)])
             randomizedIndices = [ii for ii in range(self.populationSize)]
             random.shuffle(randomizedIndices)
+            
+            path, minDistance = self._bestSolution(population)
+            self.path = path + [path[0]]
+            self.solutionHistory.append([minDistance])
         
             newPopulation = []
             for ii in range(self.populationSize // 4):
@@ -98,9 +112,9 @@ class TspGA(object):
                 else:
                     newPopulation = np.append(newPopulation, offspring, axis=0)
             population = newPopulation
-        
-        #Calculates the path with least cost
-        populationDist = np.array([sum([self.dist[population[jj][ii - 1], population[jj][ii]] for ii in range(n)]) for jj in range(self.populationSize)])
+    
+    def _bestSolution(self, population):
+        populationDist = np.array([sum([self.dist[population[jj][ii - 1], population[jj][ii]] for ii in range(len(population[0]))]) for jj in range(self.populationSize)])
         index = np.where(populationDist == populationDist.min())
-        self.path = population[index[0][0]]
+        return population[index[0][0]].tolist(), populationDist.min()
 
--- Travelling salesman problem/Genetic algorithm/TspGA_DEMO.py
+++ Travelling salesman problem/Genetic algorithm/TspGA_DEMO.py
@@ -3,23 +3,19 @@
 from TspGA_Class import TspGA
 import matplotlib.pyplot as plt
 import numpy as np
+import random as random
 
 if __name__ == '__main__':
     # Example implementation of the genetic TSP algorithm
-    x = np.array([0., 0.6, 1., 0.6, 0., -0.6, -1., -0.6,])
-    y = np.array([-1., 0., 0.9, 1.5, 1., 1.5, 0.9, 0.,])
-    tsp = TspGA(x, y)
     if 1:
+        N = 50
+        x = np.array([random.randint(0,N) for ii in range(N)])
+        y = np.array([random.randint(0,N) for ii in range(N)])
+        tsp = TspGA(x, y)
         plt.figure(1)
         tsp.plot(distance = 1)
-    if 1:
         plt.figure(2)
         tsp._genetic()
         tsp.plot(solution = 1, points = 1)
-    if 1:
         plt.figure(3)
-        x = np.array([1, 2, 3, 5, 7, 8, 6])
-        y = np.array([7, 4, 5, 1, 2, 4, 6])
-        tsp = TspGA(x, y)
-        tsp._genetic()
-        tsp.plot(solution = 1, points = 1)
+        tsp.plot(solutionHistory = 1)
